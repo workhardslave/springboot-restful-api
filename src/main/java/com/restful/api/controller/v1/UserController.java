1477f79b276f0d1f520f6c217ed692cb227d2003
workhardslave <mae0428@naver.com>
2021-03-18 13:40:59 +0900

    feat : Add swagger documentation api

--- src/main/java/com/restful/api/controller/v1/UserController.java
+++ src/main/java/com/restful/api/controller/v1/UserController.java
@@ -2,11 +2,11 @@ package com.restful.api.controller.v1;
 
 import com.restful.api.entity.User;
 import com.restful.api.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,28 +19,41 @@ import java.util.List;
  * 결과 데이터를 JSON으로 반환
  * @Controller + @ResponseBody
  *
- * @RequestMapping(value = "/v1")
+ * @RequestMapping(value = "")
  * api를 버전별로 관리하기 위해 공통된 리소스를 묶는다.
+ *
+ * @Api(tags = {""})
+ * UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅힌다.
+ *
+ * @ApiOperation(value = "", notes = "")
+ * 각각의 리소스에 제목과 설명을 표시하기위해 세팅한다
+ *
+ * @ApiParam(value = "", required = "") @RequestParam ~~~
+ * 파라미터에 대한 설명을 보여주기 위해 세팅한다.
  *
  */
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RequestMapping(value = "/v1")
-@RestController // 결과값을 JSON으로 출력합니다.
+@RestController // 결과값을 JSON으로 출력한다.
 public class UserController {
     private final UserRepository userRepository;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.")
     @GetMapping(value = "/users")
     public List<User> findAllUser() {
         System.out.println(userRepository.findAll());
         return userRepository.findAll();
     }
 
+    @ApiOperation(value = "회원 등록", notes = "회원을 등록한다.")
     @PostMapping(value = "/users")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String username) {
         User user = User.builder()
-                .uid("yumi@naver.com")
-                .username("유미")
+                .uid(uid)
+                .username(username)
                 .build();
         return userRepository.save(user);
     }
